f5567ca5ab80bb22bbc3db6e62a04dea3a2a3abf
Peter Arnold <peter@cagefreeclimbing.com>
2019-05-17 11:38:17 -0400

    material-ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import Map from "./Map";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <p>Inspector On The Loose</p>
-    </div>
+    <Container maxWidth="sm">
+      <Box my={4}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Inspector On The Loose
+        </Typography>
+        <Map />
+      </Box>
+    </Container>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
+//other
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+//material-ui
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/styles";
+import theme from "./theme";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//components
+import App from "./App";
+
+//main render
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
+    <App />
+  </ThemeProvider>,
+  document.querySelector("#root")
+);
+
+//register service worker
+serviceWorker.register();
